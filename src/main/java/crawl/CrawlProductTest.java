c6d757f1f904c5cfafc988e9de9699aa37887635
Q Q <144197743+QuanPham123123@users.noreply.github.com>
2024-11-26 19:05:44 +0700

    Quan (#5)
    * Cập nhật lại quá trình crawl
    
    Sử dụng các thông tin cấu hình từ config database
    
    * Lớp test class CrawlProduct
    
    Lớp test class CrawlProduct
    
    * Tạo phương thức runCrawl thực hiện việc crawl data
    
    Tạo phương thức runCrawl thực hiện việc crawl data
    
    * Revert "Tạo phương thức runCrawl thực hiện việc crawl data"
    
    This reverts commit ee199fb84d9faf2e131f5375a423d0340ce3ca4f.
    
    * Tạo phương thức runCrawl để thực hiện việc crawl data

---
+++ src/main/java/crawl/CrawlProductTest.java
@@ -0,0 +1,11 @@
+package crawl;
+
+
+
+public class CrawlProductTest {
+    public static void main(String[] args) {
+        CrawlProduct crawl = new CrawlProduct();
+        crawl.runCrawl();
+    }
+}
+
